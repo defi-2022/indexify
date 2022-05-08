6537c6100d01c15a2f5b588e3349c6a6dd33dff8
nikoferro <nicolaspatricioferro@gmail.com>
2022-05-08 18:19:11 +0200

    chore: undoing monorepo setup

--- packages/react/src/pages/create/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Create() {
-  return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>Create</h2>
-    </main>
-  );
-}
--- packages/react/src/pages/invest/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Invest() {
-  return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>Invest</h2>
-    </main>
-  );
-}
