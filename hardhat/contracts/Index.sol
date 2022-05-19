6710e27a7b99acdffcf21932ed6fd5ae0ad7e713
nikoferro <nicolaspatricioferro@gmail.com>
2022-05-19 20:55:09 +0200

    feat: v1 of the deployer + index contract ready to test

--- hardhat/contracts/Index.sol
+++ hardhat/contracts/Index.sol
@@ -8,17 +8,13 @@ import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-interface IUniswapRouter is ISwapRouter {
-    function refundETH() external payable;
-}
-
 contract Index is ERC20 {
     using SafeMath for uint256;
     using SafeERC20 for ERC20;
 
     // Uniswap V3 addressses are the same on all networks
-    IUniswapRouter public constant ROUTER =
-        IUniswapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
+    ISwapRouter public constant ROUTER =
+        ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
     IQuoter public constant QUOTER =
         IQuoter(0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6);
 
@@ -26,38 +22,161 @@ contract Index is ERC20 {
     /// name is handled by ERC20 openzeppelin contract
     /// symbol is handled by ERC20 openzeppelin contract
     address[] public composition;
-    uint256[] public tokenDecimals;
     uint256[] public percentages;
-    address public manager;
+    uint24[] public poolFees;
+    address payable public managerAddress;
     uint256 public managerFee;
+    uint256 public protocolFee;
+    address payable public protocolAddress = payable(address(0));
     address public networkCurrency;
 
-    // Index vault
+    // Index vault (investor => token => amount)
     mapping(address => mapping(address => uint256)) public vault;
+
+    uint256 public protocolBalance = 0;
+    uint256 public managerBalance = 0;
+
+    // Events
+    event Invested(address investor, uint256 amount);
+    event Redeemed(address investor, uint256 amount);
 
     constructor(
         string memory _name,
         string memory _symbol,
         address[] memory _composition,
-        uint256[] memory _tokenDecimals,
         uint256[] memory _percentages,
-        address _manager,
+        uint24[] memory _poolFees,
         uint256 _managerFee,
-        address _networkCurrency
+        address _nativeCurrency,
+        uint256 _protocolFee,
+        address _protocolAddress,
+        address _manager
     ) ERC20(_name, _symbol) {
         composition = _composition;
-        tokenDecimals = _tokenDecimals;
+        // percentages should use basis points
         percentages = _percentages;
-        manager = _manager;
+        poolFees = _poolFees;
         managerFee = _managerFee;
-        networkCurrency = _networkCurrency;
+        networkCurrency = _nativeCurrency;
+        managerAddress = payable(_manager);
+        protocolFee = _protocolFee;
+        protocolAddress = payable(_protocolAddress);
+        // validate lengths
+        require(
+            _composition.length == _percentages.length &&
+                _composition.length == _poolFees.length,
+            "composition, decimals, percentages, and poolFees must be the same length"
+        );
 
-        // validate composition
-        // validate that all tokens are unique
-        // validate that all addresses are ERC20
-        // validate that all decimals are positive
-        // validate that all percentages are positive
-        // validate that all percentages add up to 100
-        // validate that all the lists are the same length
+        // validate that all percentages add up to 10000 basis points
+        uint256 totalPercentage = 0;
+        for (uint256 i = 0; i < _percentages.length; i++) {
+            totalPercentage = totalPercentage + _percentages[i];
+        }
+        require(
+            totalPercentage == 10000,
+            "percentages must add up to 10000 basis points"
+        );
+    }
+
+    function buy() public payable {
+        //divide the amount by the total percentage in basis points
+        require(msg.value > 0, "must send ether to buy");
+
+        // calculate fees
+        uint256 _total = 0;
+        uint256[] memory _amounts;
+        uint256 _feeForManager = (msg.value * managerFee) / 10000;
+        uint256 _feeForProtocol = (msg.value * managerFee) / 10000;
+        uint256 _valueAfterFees = msg.value - _feeForManager - _feeForProtocol;
+
+        // calculate amounts of ETH to swap
+        for (uint256 i = 0; i < composition.length; i++) {
+            require(
+                (_valueAfterFees / 10000) * 10000 == _valueAfterFees,
+                "eth sent is too small to calculate amounts"
+            );
+            uint256 _amountToBuy = (_valueAfterFees * percentages[i]) / 10000;
+            _amounts[i] = _amountToBuy;
+            _total = _total + _amountToBuy;
+        }
+
+        uint256 _reminder = _total - _valueAfterFees;
+
+        // swap ETH for tokens
+        for (uint256 i = 0; i < composition.length; i++) {
+            uint256 _amountToBuy = _amounts[i];
+            require(_amountToBuy > 0, "amount to buy must be greater than 0");
+            // swap ETH for token
+            uint256 amountSwapped = swapExactETHForToken(
+                _amountToBuy,
+                composition[i],
+                poolFees[i] // to match the proper pool from where to get the liquidity
+            );
+            // add to vault
+            vault[msg.sender][composition[i]] =
+                vault[msg.sender][composition[i]] +
+                amountSwapped;
+        }
+
+        // fractional reminder goes to protocol balance
+        if (_reminder > 0) {}
+    }
+
+    function redeem() external {
+        // redeem everything that belongs to him in the vault and burn its tokens
+        for (uint256 i = 0; i < composition.length; i++) {
+            ERC20(composition[i]).safeTransfer(
+                msg.sender,
+                vault[composition[i]][msg.sender]
+            );
+        }
+        _burn(msg.sender, balanceOf(msg.sender));
+    }
+
+    function withdrawFees() external payable {
+        if (msg.sender == managerAddress) {
+            managerAddress.transfer(managerBalance);
+            managerBalance = 0;
+        }
+
+        if (msg.sender == protocolAddress) {
+            protocolAddress.transfer(protocolBalance);
+            protocolBalance = 0;
+        }
+    }
+
+    function swapExactETHForToken(
+        uint256 _amountIn,
+        address _tokenOut,
+        uint24 _fee
+    ) private returns (uint256 amountOut) {
+        require(_amountIn > 0, "Must pass non 0 ETH amount");
+        require(_fee > 0, "Must pass non 0 fee amount");
+
+        // using 'now' for convenience, for mainnet pass deadline from frontend!
+        uint256 deadline = block.timestamp + 15;
+
+        address tokenIn = networkCurrency;
+        address recipient = address(this);
+
+        // tweak this values for mainnet launch
+        uint160 sqrtPriceLimitX96 = 0;
+        uint160 amountOutMinimum = 0;
+
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
+            .ExactInputSingleParams(
+                tokenIn,
+                _tokenOut,
+                _fee,
+                recipient,
+                deadline,
+                _amountIn,
+                amountOutMinimum,
+                sqrtPriceLimitX96
+            );
+        // should return reminder of eth? in this case it does not matter
+        // since all eth sent is used
+        return ROUTER.exactInputSingle{value: _amountIn}(params);
     }
 }
--- hardhat/contracts/IndexDeployer.sol
+++ hardhat/contracts/IndexDeployer.sol
@@ -4,23 +4,29 @@ pragma solidity ^0.8.0;
 import "./Index.sol";
 
 contract IndexDeployer {
-    address owner;
+    address protocolAddress;
     address[] public indexes;
+    uint256 public indexContractVersion = 0;
+    uint256 protocolFee = 100; // default 1%
 
     event LogDeployedIndexContract(address child);
 
     mapping(uint256 => address) nativeCurrenciesByChainId;
 
-    constructor() {
-        owner = msg.sender;
+    Index public IndexContract = Index(address(0));
+
+    constructor(address _indexContractAddress) {
+        // msg sender here is the protocol address
+        protocolAddress = msg.sender;
+        setNewContractVersion(_indexContractAddress);
     }
 
     function createIndex(
         string memory _name,
         string memory _symbol,
         address[] memory _composition,
-        uint256[] memory _decimals,
         uint256[] memory _percentages,
+        uint24[] memory _poolFees,
         uint256 _managerFee
     ) public {
         address _nativeCurrency = nativeCurrenciesByChainId[block.chainid];
@@ -32,26 +38,34 @@ contract IndexDeployer {
             _name,
             _symbol,
             _composition,
-            _decimals,
             _percentages,
-            msg.sender,
+            _poolFees,
             _managerFee,
-            _nativeCurrency
+            _nativeCurrency,
+            protocolFee,
+            protocolAddress,
+            msg.sender
         );
         emit LogDeployedIndexContract(address(index));
         indexes.push(address(index));
     }
 
-    function setOwner(address _owner) public {
-        require(msg.sender == owner);
-        owner = _owner;
+    function setProtocolAddress(address _protocolAddress) public {
+        require(msg.sender == protocolAddress);
+        protocolAddress = _protocolAddress;
+    }
+
+    function setNewContractVersion(address _newVersion) public {
+        require(msg.sender == protocolAddress);
+        IndexContract = Index(_newVersion);
+        indexContractVersion = indexContractVersion + 1;
     }
 
     function setNativeCurrencyForChainId(
         address _nativeCurrency,
         uint256 _chainId
     ) public {
-        require(msg.sender == owner);
+        require(msg.sender == protocolAddress);
         nativeCurrenciesByChainId[_chainId] = _nativeCurrency;
     }
 }
